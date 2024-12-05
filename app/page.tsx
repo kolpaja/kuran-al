4675f3e8f53397268ea9e7b5a333df7255ec4d07
Sokol Paja <kolpaja28@gmail.com>
2024-12-05 10:12:07 +0100

    comming soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return (
-    <div className='flex justify-center items-center pt-20'>
-      <h1 className='text-3xl font-bold'>Kuran.al vjen se shpejti</h1>
-    </div>
-  );
+	return (
+		<div className='flex justify-center items-center pt-20 flex-col gap-6'>
+			<Image src='/img/quran.jpg' alt='quran' className='rounded-full p-1' width={500} height={800} />
+			<h1 className='text-3xl font-bold'>Kuran.al vjen se shpejti</h1>
+		</div>
+	);
 }
